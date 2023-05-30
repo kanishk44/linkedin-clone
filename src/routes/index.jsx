d911fea2acb5117fae30b4b15669117e8aefb6d1
kanishk44 <ks468@njit.edu>
2023-05-30 11:52:20 -0400

    changed import name in routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import HomeLayout from "../layouts/homeLayout";
-import ProfileLayout from "../layouts/profileLayout";
+import HomeLayout from "../layouts/HomeLayout";
+import ProfileLayout from "../layouts/ProfileLayout";
 
 export const router = createBrowserRouter([
   {
